747a3def1f3e762f552375447f8f9e11280a5bfb
melkinsgit <margaret.elkins@gmail.com>
2016-03-19 18:01:28 -0500

    All working

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -11,6 +11,12 @@ $(document).ready(function() {
     $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 	// Add User button click
     $('#btnAddUser').on('click', addUser);
+	// Delete User link click
+    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+	// Edit User link click
+    $('#userList table tbody').on('click', 'td a.linkedituser', editUser);
+	// Update User link click
+    $('#btnEditUser').on('click', updateUser);	
 
 });
 
@@ -29,11 +35,13 @@ function populateTable() {
 		userListData = data
 
         // For each item in our JSON, add a table row and cells to the content string
+		// class identifies method to call, rel identifies user name
         $.each(data, function(){
             tableContent += '<tr>';
             tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
             tableContent += '<td>' + this.email + '</td>';
             tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+            tableContent += '<td><a href="#" class="linkedituser" rel="' + this._id + '">edit</a></td>';
             tableContent += '</tr>';
         });
 
@@ -47,14 +55,17 @@ function showUserInfo(event) {
 
     // Prevent Link from Firing
     event.preventDefault();
+	
+	// in case the user has clicked on a table item to edit, but not submitted changes
+	$('#editUser fieldset input').val('');
 
     // Retrieve username from link rel attribute
     var thisUserName = $(this).attr('rel');
 
-    // Get Index of object based on id value
+    // Get Index of object based on username value
     var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
 	
-	    // Get our User Object
+	// Get our User Object
     var thisUserObject = userListData[arrayPosition];
 
     //Populate Info Box
@@ -90,7 +101,6 @@ function addUser(event) {
             'location': $('#addUser fieldset input#inputUserLocation').val(),
             'gender': $('#addUser fieldset input#inputUserGender').val()
         }
-		console.log(newUser);
 
         // Use AJAX to post the object to our adduser service; the adduser fn in the users js file with insert the newUser - a JSON object of data - into the database
         $.ajax({
@@ -108,6 +118,153 @@ function addUser(event) {
 
                 // Update the table
                 populateTable();
+
+            }
+            else {
+
+                // If something goes wrong, alert the error message that our service returned; the push to the db didn't work
+                alert('Error: ' + response.msg);
+
+            }
+        });
+    }
+    else {  // the user didn't fill in all the field
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+};
+
+// Delete User
+function deleteUser(event) {
+
+    event.preventDefault();
+	
+	// in case the user has clicked on a table item to edit, but not submitted changes
+	$('#editUser fieldset input').val('');
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+
+        // If they did, do our delete - ajax DELETE call with the id attribute of the calling link, just like the show user - in show user we're getting the array position of the user, here we are deleting the user via the ajax call to colleciton.remove in deleteuser
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function( response ) {
+			
+			// the response is a message that is null if all OK or the appropriate error msg
+
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateTable();
+
+        });
+
+    }
+    else {
+
+        // If they said no to the confirm, do nothing
+        return false;
+
+    }
+
+};
+
+// Edit user
+function editUser (event){
+	
+	event.preventDefault();
+		
+	// Retrieve username from link rel attribute
+	var thisUserID = $(this).attr('rel');
+	
+	// Get Index of object based on id value
+	var arrayPosition = userListData.map(function(arrayItem) { return arrayItem._id; }).indexOf(thisUserID);
+	
+	// Get our User Object
+	var thisUserObject = userListData[arrayPosition];
+	
+	// inject object into existing fieldset for edit user; populate edit text boxes
+	$('#editUser fieldset input#inputUserName').val(thisUserObject.username);
+	$('#editUser fieldset input#inputUserEmail').val(thisUserObject.email);
+	$('#editUser fieldset input#inputUserFullname').val(thisUserObject.fullname);
+	$('#editUser fieldset input#inputUserAge').val(thisUserObject.age);
+	$('#editUser fieldset input#inputUserLocation').val(thisUserObject.location);
+	$('#editUser fieldset input#inputUserGender').val(thisUserObject.gender);
+	
+	
+	// Anna had the clever solution of adding the id field (hidden) to her dual addUser/editUser fieldset; I tried everything I could to avoid it, but in the end I needed something the user wouldn't/couldn't change to access the doc in the collection that I wanted to update when the user was done, id number (PK) was the obvious choice
+	$('#editUser fieldset input#inputUserID').val(thisUserObject._id);
+        
+};
+
+// Update user
+function updateUser (event){
+	event.preventDefault();
+	
+	// the ability to get the doc by id is based on the fact that id was added to the edit field set
+	var idToEdit = $('#editUser fieldset input#inputUserID').val();
+	console.log(idToEdit);
+	
+	// Get Index of object based on username value
+    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem._id; }).indexOf(idToEdit);
+	console.log(arrayPosition);
+	
+	// Get our User Object
+    var thisUserObject = userListData[arrayPosition];
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+	var errorCount = 0;
+	// for the input associated with the add user button, cycle through the input boxes
+	$('#editUser input').each(function (index, val){
+		// if the input value of any given input box is null, increment the error county
+		if ($(this).val() === '' )
+			{errorCount++;
+			console.log($(this).val());}
+	});
+
+    // Check and make sure errorCount's still at zero, which means all boxes have been filled with text
+    if(errorCount === 0) {
+
+        // If it is, compile all user info into one object, instantiate the object with the values from teh input boxes with the appropriate names
+        var newUser = {
+            'username': $('#editUser fieldset input#inputUserName').val(),
+            'email': $('#editUser fieldset input#inputUserEmail').val(),
+            'fullname': $('#editUser fieldset input#inputUserFullname').val(),
+            'age': $('#editUser fieldset input#inputUserAge').val(),
+            'location': $('#editUser fieldset input#inputUserLocation').val(),
+            'gender': $('#editUser fieldset input#inputUserGender').val()
+        }
+
+        // Use AJAX to put the object to our updateuser service; the updateuser fn in the users js file with insert the newUser - a JSON object of data - into the database
+		console.log('id num for put is ' + thisUserObject._id);
+        $.ajax({
+            type: 'PUT',
+            data: newUser,
+            url: '/users/updateuser/' + thisUserObject._id,
+            dataType: 'JSON'
+        }).done(function( response ) {
+
+            // Check for successful (blank) response from the ajax call
+            if (response.msg === '') {
+				
+				console.log('we should have accomplished the update');
+
+                // Clear the form inputs
+                $('#editUser fieldset input').val('');
+
+                // Update the table
+                populateTable();
+				clearVars();
 
             }
             else {
--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,45 @@ router.get('/userlist', function( req, res) {
 	} );  // end get from collection
 } );  // end get from router
 
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+/*
+ * PUT to updateuser.
+ */
+router.put('/updateuser/:id', function(req, res) {
+	var db = req.db;
+	var collection = db.get('userlist');
+	var userToUpdate = req.params.id;
+	// I stumbled around for awhile with mongo update, and then noticed that Anna just updated the doc body - it's all JSON! that's why it's so easy!
+    var userDataToUpdate = req.body;
+    collection.update({'_id':userToUpdate}, userDataToUpdate, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
 module.exports = router;
